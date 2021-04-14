d4fbde61b4ebf5b08ff5d02c604db09a0b2e7b48
Ernstbert <joachim.horvath@student.tugraz.at>
2021-04-14 11:55:39 +0200

    T-012 refactored menu item name [JH, VH]

--- TEACHomatic3000/app/src/main/java/com/example/teachomatic3000/ui/Datum/DatumFragment.kt
+++ TEACHomatic3000/app/src/main/java/com/example/teachomatic3000/ui/Datum/DatumFragment.kt
@@ -1,4 +1,4 @@
-package com.example.teachomatic3000.ui.slideshow
+package com.example.teachomatic3000.ui.Datum
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.teachomatic3000.R
 
-class SlideshowFragment : Fragment() {
+class DatumFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
+    private lateinit var datumViewModel: DatumViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-                ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+        datumViewModel =
+                ViewModelProvider(this).get(DatumViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_datum, container, false)
+        val textView: TextView = root.findViewById(R.id.text_datum)
+        datumViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- TEACHomatic3000/app/src/test/java/com/example/teachomatic3000/ExampleUnitTest.kt
+++ TEACHomatic3000/app/src/test/java/com/example/teachomatic3000/ExampleUnitTest.kt
@@ -14,4 +14,23 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    /*
+    @Test
+    fun student_Test() {
+        // hier kann ich auf alle klassen zugreifen
+        val student = StudentModel("Vorname", "Nachname")
+            assertEquals(student.vorname, "vorname")
+    }
+    */
+    // F0: Wenn "Settings" geklickt -> Zeige leeren view (mit button "Datum einstellen")
+    // F1: man kann auswählen welcher Tag ist
+    // F2: Button zum auswählen des Tags. Wenn Eingabe = 0, system Uhrzeit
+
+    // Wir brauchen "Einstellungen", in denen wir Datum einstellen können
+    // In der "ui" ein Fragment... oder einen view, für das Datum
+    // im mainactivity.kt einen eventlistener für calendar
+
+
+
 }
