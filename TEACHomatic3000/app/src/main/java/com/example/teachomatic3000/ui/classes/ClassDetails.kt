388929c1cb772569f95a3ff4e017b03551223ca2
Marvin Rantschl <marvin.rantschl@student.tugraz.at>
2021-05-26 09:53:37 +0200

    T-022 added ClassDetails SuS List logic and small layout change [HV, RM]

--- TEACHomatic3000/app/src/main/java/com/example/teachomatic3000/ui/classes/ClassDetails.kt
+++ TEACHomatic3000/app/src/main/java/com/example/teachomatic3000/ui/classes/ClassDetails.kt
@@ -9,6 +9,7 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.teachomatic3000.R
 import com.example.teachomatic3000.database.DataBaseHelper
+import com.example.teachomatic3000.models.ClassModel
 import com.example.teachomatic3000.ui.lehrstoff.LehrstoffKlassenHelper
 import kotlin.properties.Delegates
 
@@ -24,7 +25,10 @@ class ClassDetails : AppCompatActivity() {
     private lateinit var lehrstoff_liste: ListView
     private lateinit var lehrstoffListAdapter: ArrayAdapter<String>
     private lateinit var pruefungListAdapter: ArrayAdapter<String>
+    private lateinit var schuelerKlassenListAdapter: ArrayAdapter<String>
     private lateinit var pruefungList: ListView
+    private lateinit var schueler_klassen_liste: ListView
+    private lateinit var classModel: ClassModel
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,10 +39,16 @@ class ClassDetails : AppCompatActivity() {
         db = DataBaseHelper(this.baseContext)
         updateClassInfoText()
 
+        classModel = db.getClass(class_id)!!
+
         pruefung_erstellen = findViewById(R.id.pruefung_erstellen)
         button_add_student_to_class = findViewById(R.id.button_add_student_to_class)
         button_anonymize_class = findViewById(R.id.anonymize_class_button)
         create_lehrstoff_button = findViewById(R.id.create_lehrstoff_button)
+
+        schueler_klassen_liste = findViewById(R.id.schueler_klassen_liste)
+        schuelerKlassenListAdapter = ArrayAdapter(this.baseContext, android.R.layout.simple_list_item_1, db.getStudentsOfClass(classModel))
+        schueler_klassen_liste.adapter = schuelerKlassenListAdapter
 
         pruefungList = findViewById(R.id.pruefung_listview)
         pruefungListAdapter = ArrayAdapter(this.baseContext, android.R.layout.simple_list_item_1, db.getPruefung(class_id, this.baseContext))
@@ -93,6 +103,18 @@ class ClassDetails : AppCompatActivity() {
             startActivity(intent)
         }
 
+        schueler_klassen_liste.setOnTouchListener(OnTouchListener { v, event ->
+            val action = event.action
+            when (action) {
+                MotionEvent.ACTION_DOWN ->
+                    v.parent.requestDisallowInterceptTouchEvent(true)
+                MotionEvent.ACTION_UP ->
+                    v.parent.requestDisallowInterceptTouchEvent(false)
+            }
+            v.onTouchEvent(event)
+            true
+        })
+
         lehrstoff_liste.setOnTouchListener(OnTouchListener { v, event ->
             val action = event.action
             when (action) {
@@ -125,6 +147,8 @@ class ClassDetails : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        schuelerKlassenListAdapter = ArrayAdapter(this.baseContext, android.R.layout.simple_list_item_1, db.getStudentsOfClass(classModel))
+        schueler_klassen_liste.adapter = schuelerKlassenListAdapter
         pruefungListAdapter = ArrayAdapter(this.baseContext, android.R.layout.simple_list_item_1, db.getPruefung(class_id, this.baseContext))
         pruefungList.adapter = pruefungListAdapter
         lehrstoffListAdapter = ArrayAdapter(this.baseContext, android.R.layout.simple_list_item_1, db.getLehrstoffeForKlasse(class_id, this.baseContext))
